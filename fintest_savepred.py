a617a9e93f850dbbc90fb8815124d5c5c7578b5f
keyboard1st <157776579@qq.com>
2025-05-27 16:21:24 +0800

    refactor(fintest): 重构模型测试和预测流程
    -将模型测试和预测相关代码从 fintest_savepred.py 移动到新的 models_pred.py 文件
    - 优化了 test_all_model 和 pred_all_model 函数，增加了 PathConfig 参数
    - 更新了 model_mixer 函数，现在支持传入 market_cap_df 和 labels_df 参数
    - 改进了 save_pred 函数，增加了 PathConfig 参数并优化了保存逻辑
    - 删除了 fintest_savepred.py 中的冗余代码，提高了代码可读性和可维护性

--- fintest_savepred.py
+++ fintest_savepred.py
@@ -1,6 +1,5 @@
-import itertools
+
 import warnings
-from copy import deepcopy
 
 import lightgbm as lgb
 import xgboost as xgb
@@ -8,11 +7,10 @@ from catboost import CatBoostRegressor
 
 from metrics.backtest import *
 from metrics.calculate_ic import ic_between_models_plot
+from metrics.models_pred import *
 from model.GRU_attention import AttGRU
 from model.GRU_model import *
 from model.TimeMixer import TimeMixer
-from train.GBDT_trainer import tree_test, tree_pred
-from train.GRU_cross_time_train import GRU_fin_test, GRU_pred_market
 
 warnings.filterwarnings("ignore", category=RuntimeWarning)
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -23,7 +21,7 @@ from config import get_config
 
 # ─── Paths & Config ───────────────────────────────────────────────────────────
 config = get_config()
-
+config.task_name = 'Jin_2021_2022_randomseed2025'
 config.exp_path = f'/home/hongkou/TimeSeries/exp/{config.task_name}'
 print('fin pred exp path',config.exp_path)
 class PathConfig:
@@ -34,7 +32,7 @@ class PathConfig:
 
     # model weights
     GRU      = os.path.join(exp_path,   'models/best_model.pth')
-    use_minute_model = False
+    use_minute_model = True
     TimeMixer   = os.path.join('/home/hongkou/TimeSeries/exp/minute10_2021_2022/models/best_model.pth')
     XGB      = os.path.join(exp_path,   'models/xgb.xgb')
     LGB      = os.path.join(exp_path,   'models/lgbm.txt')
@@ -109,148 +107,13 @@ else:
         "CatBoost": cat_model
     }
 
-def test_all_model(TimeSeries_xy_loader, CrossSection_xy_loader, Minute_xy_loader = None, **models):
-    model_pred_dict = {}  # 用来存各个模型的 pred_arr
-    for name, model in models.items():
-        if name == 'GRU':
-            print('----------------------GRU test--------------------------')
-            GRU_pred_arr, GRU_true = GRU_fin_test(TimeSeries_xy_loader, model)
-            print("GRU pred shape ", GRU_pred_arr.shape)
-            print("GRU true shape ", GRU_true.shape)
-            model_pred_dict[name] = GRU_pred_arr
-            model_pred_dict['label'] = GRU_true
-        elif name == 'TimeMixer' and PathConfig.use_minute_model:
-            print('----------------------TimeMixer test-----------------------')
-            Minute_pred_arr, Minute_true = GRU_fin_test(Minute_xy_loader, model)
-            print("Minute pred shape ", Minute_pred_arr.shape)
-            print("Minute true shape ", Minute_true.shape)
-            model_pred_dict[name] = Minute_pred_arr
-        else:
-            print(f'----------------------{name} test-----------------------')
-            cur_loader = deepcopy(CrossSection_xy_loader)
-            tree_pred_arr, tree_true = tree_test(cur_loader, model)
-            print(f"{name} pred shape ", tree_pred_arr.shape)
-            print(f"{name} true shape ", tree_true.shape)
-            model_pred_dict[name] = tree_pred_arr
-
-    return model_pred_dict
-
-def pred_all_model(TimeSeries_x_loader, CrossSection_x_loader, index_and_clos, Minute_x_loader = None, **models):
-    model_pred_df_dict = {}
-    for name, model in models.items():
-        if name == 'GRU':
-            print('----------------------GRU Pred--------------------------')
-            GRU_pred_arr = GRU_pred_market(TimeSeries_x_loader, model)
-            print("GRU pred shape ", GRU_pred_arr.shape)
-            GRU_fin_pred_df = pd.DataFrame(GRU_pred_arr)
-            GRU_fin_pred_df.index = index_and_clos.index
-            GRU_fin_pred_df.columns = index_and_clos.columns
-            model_pred_df_dict[name] = GRU_fin_pred_df
-        elif name == 'TimeMixer' and PathConfig.use_minute_model:
-            print('----------------------TimeMixer Pred-----------------------')
-            Minute_pred_arr = GRU_pred_market(Minute_x_loader, model)
-            print("Minute pred shape ", Minute_pred_arr.shape)
-            Minute_fin_pred_df = pd.DataFrame(Minute_pred_arr)
-            Minute_fin_pred_df.index = index_and_clos.index
-            Minute_fin_pred_df.columns = index_and_clos.columns
-            model_pred_df_dict[name] = Minute_fin_pred_df
-        else:
-            print(f'----------------------{name} Pred-----------------------')
-            cur_loader = deepcopy(CrossSection_x_loader)
-            tree_pred_arr = tree_pred(cur_loader, model)
-            print(f"{name} pred shape ", tree_pred_arr.shape)
-            tree_pred_df = pd.DataFrame(tree_pred_arr)
-            tree_pred_df.index = index_and_clos.index
-            tree_pred_df.columns = index_and_clos.columns
-            model_pred_df_dict[name] = tree_pred_df
-
-    model_pred_df_dict['GBDT'] = (model_pred_df_dict['XGBoost'] + model_pred_df_dict['LightGBM'] + model_pred_df_dict['CatBoost']) / 3
-    model_pred_df_dict.pop('XGBoost',None)
-    model_pred_df_dict.pop('LightGBM',None)
-    model_pred_df_dict.pop('CatBoost',None)
-
-    return model_pred_df_dict
-
-def get_weights(n):
-    '''
-    返回将1拆成n个加和为1的权重列表，每个权重为0.1的倍数，例如n=3时：
-    [[0.1, 0.1, 0.8], [0.1, 0.2, 0.7], ...]
-    '''
-    steps = [i / 10 for i in range(1, 10)]  # 0.1 到 0.9（不包含0）
-    weights = []
-    for w in itertools.product(steps, repeat=n):
-        if abs(sum(w) - 1.0) < 1e-8:
-            weights.append(list(w))
-    return weights
-
-def model_mixer(model_pred_df_dict):
-    print('----------------------model mixer-----------------------')
-    print('base models:', model_pred_df_dict.keys())
-    if PathConfig.use_minute_model:
-        avg_pred = (model_pred_df_dict['GRU'] + model_pred_df_dict['TimeMixer'] + model_pred_df_dict['GBDT']) / 3.0
-        model_pred_df_dict['0.33GRU & 0.33TimeMixer & 0.33GBDT'] = avg_pred
-        weights = get_weights(3)
-        ic_records = []
-        for w1, w2, w3 in weights:
-            # 加权预测
-            mix_pred = (
-                    w1 * model_pred_df_dict['GRU'] +
-                    w2 * model_pred_df_dict['TimeMixer'] +
-                    w3 * model_pred_df_dict['GBDT']
-            )
-            # 只在市值>0的位置计算 IC
-            masked = mix_pred.where(market_cap_df > 0)
-            # 按列（股票）计算与 labels 的相关，再取平均
-            ic = (masked.T.corrwith(labels_df.T)).mean()
-            ic_records.append(((w1, w2, w3), ic, mix_pred))
-            print(f'{w1}GRU & {w2}TimeMixer & {w3}GBDT, IC = {ic:.4f}')
-
-        top5 = sorted(ic_records, key=lambda x: x[1], reverse=True)[:5]
-        for idx, ((w1, w2, w3), ic, pred_df) in enumerate(top5, 1):
-            name = f'{w1}GRU & {w2}TimeMixer & {w3}GBDT'
-            print(f'top{idx} {name}, IC = {ic:.4f}')
-            model_pred_df_dict[name] = pred_df
-        print('final models:', list(model_pred_df_dict.keys()))
-
-    else:
-        weights = get_weights(2)
-        ic_records = []
-        for w1, w2 in weights:
-            mix_pred = (w1 * model_pred_df_dict['GRU'] + w2 * model_pred_df_dict['GBDT'])
-            # 只在市值>0的位置计算 IC
-            masked = mix_pred.where(market_cap_df > 0)
-            # 按列（股票）计算与 labels 的相关，再取平均
-            ic = (masked.T.corrwith(labels_df.T)).mean()
-            ic_records.append(((w1, w2), ic, mix_pred))
-            print(f'{w1}GRU & {w2}GBDT, IC = {ic:.4f}')
-        top5 = sorted(ic_records, key=lambda x: x[1], reverse=True)[:5]
-        for idx, ((w1, w2), ic, pred_df) in enumerate(top5, 1):
-            name = f'{w1}GRU & {w2}GBDT'
-            print(f'top{idx} {name}, IC = {ic:.4f}')
-            model_pred_df_dict[name] = pred_df
-        print('final models:', list(model_pred_df_dict.keys()))
-
-
-    return model_pred_df_dict
-
-def save_pred(model_pred_df_dict):
-    model_pred_df_dict_with_ic = {}
-    for name, pred_df in model_pred_df_dict.items():
-        pred_df_mask = pred_df.where(market_cap_df>0)
-        pred_df_mask.to_csv(os.path.join(PathConfig.save_path, f'{name}_fin_pred_mask.csv'))
-        pred_ic = (pred_df_mask.T.corrwith(labels_df.T)).mean()
-        print(f'{name} Pred ic: {pred_ic:.4f}')
-        new_name = name + str(format(pred_ic, '.4f'))
-        model_pred_df_dict_with_ic[new_name] = pred_df_mask
-    return model_pred_df_dict_with_ic
-
 
 if __name__ == '__main__':
     # from get_data.CY312.CY312_rollingtrain_dataloader import get_CY312_rollingfintest_TimeSeriesloader, get_CY312_rollingfintest_CrossSectionLoader
     # from get_data.CY312.CY312_rollingtrain_dataloader import get_CY312_rollingtrain_TimeSeriesLoader, get_CY312_rollingtrain_CrossSectionLoader
     from get_data.DrJin129.DrJin129_rollingtrain_dataloader import get_DrJin129_rollingtrain_TimeSeriesLoader, get_DrJin129_rollingtrain_CrossSectionDatasetLoader
     from get_data.DrJin129.DrJin129_rollingtrain_dataloader import get_DrJin129_rollingfintest_TimeSeriesLoader, get_DrJin129_rollingfintest_CrossSectionDatasetLoader
-    # from get_data.minute_factors.min_CS_dataloader import get_min10_rollingtrain_TimeSeriesLoader, get_min10_rollingfintest_TimeSeriesloader
+    from get_data.minute_factors.min_CS_dataloader import get_min10_rollingtrain_TimeSeriesLoader, get_min10_rollingfintest_TimeSeriesloader
 
     _, _, GRU_label_align_xy_loader = get_DrJin129_rollingtrain_TimeSeriesLoader(time_period=PathConfig.time_period, config=config)
     _, tree_label_align_xy_loader = get_DrJin129_rollingtrain_CrossSectionDatasetLoader(time_period=PathConfig.time_period)
@@ -258,15 +121,15 @@ if __name__ == '__main__':
     GRU_mkt_align_x_loader = get_DrJin129_rollingfintest_TimeSeriesLoader(time_period=PathConfig.time_period, config=config)
     trees_mkt_align_x_loader = get_DrJin129_rollingfintest_CrossSectionDatasetLoader(time_period=PathConfig.time_period)
 
-    # _, _, Minute_label_align_xy_loader = get_min10_rollingtrain_TimeSeriesLoader(time_period=PathConfig.time_period, config=config)
-    # Minute_mkt_align_x_loader = get_min10_rollingfintest_TimeSeriesloader(time_period=PathConfig.time_period, config=config)
+    _, _, Minute_label_align_xy_loader = get_min10_rollingtrain_TimeSeriesLoader(time_period=PathConfig.time_period, config=config)
+    Minute_mkt_align_x_loader = get_min10_rollingfintest_TimeSeriesloader(time_period=PathConfig.time_period, config=config)
 
     # test all models and caluculate TEST ic
-    model_pred_dict = test_all_model(GRU_label_align_xy_loader, tree_label_align_xy_loader, **models)
+    model_pred_dict = test_all_model(PathConfig, GRU_label_align_xy_loader, tree_label_align_xy_loader, Minute_label_align_xy_loader, **models)
     ic_between_models_plot(model_pred_dict, PathConfig.plot_path)
-    model_pred_df_dict = pred_all_model(GRU_mkt_align_x_loader, trees_mkt_align_x_loader, df_index_and_clos, **models)
-    model_pred_df_dict = model_mixer(model_pred_df_dict)
-    model_pred_df_dict_with_ic = save_pred(model_pred_df_dict)
+    model_pred_df_dict = pred_all_model(PathConfig, GRU_mkt_align_x_loader, trees_mkt_align_x_loader, Minute_mkt_align_x_loader, df_index_and_clos, **models)
+    model_pred_df_dict = model_mixer(PathConfig, model_pred_df_dict, market_cap_df, labels_df)
+    model_pred_df_dict_with_ic = save_pred(PathConfig, model_pred_df_dict, market_cap_df, labels_df)
 
     # 开始回测
     backtest_res = {}
