e8a032529563c7734f54bf096c97bf0758f6fb40
keyboard1st <157776579@qq.com>
2025-07-15 16:23:16 +0800

    更新模型训练演示脚本

--- models_train_demo.sh
+++ models_train_demo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-nohup python -u /home/hongkou/TimeSeries/GRU_GBDT_rollingtrain.py --task_name 'Jin_2021_2022_randomseed2025' --time_period '2021-2022' --exp_path '/home/hongkou/TimeSeries/exp/Jin_2021_2022_randomseed2025' --device 'cuda:1' --random_seed 2025 > Ztest_2021_2022.log 2>&1 &
-nohup python -u /home/hongkou/TimeSeries/GRU_GBDT_rollingtrain.py --task_name 'Jin_2022_2023_randomseed2025' --time_period '2022-2023' --exp_path '/home/hongkou/TimeSeries/exp/Jin_2022_2023_randomseed2025' --device 'cuda:2' --random_seed 2025 > Ztest_2022_2023.log 2>&1 &
-nohup python -u /home/hongkou/TimeSeries/GRU_GBDT_rollingtrain.py --task_name 'Jin_2023_2024_randomseed2025' --time_period '2023-2024' --exp_path '/home/hongkou/TimeSeries/exp/Jin_2023_2024_randomseed2025' --device 'cuda:3' --random_seed 2025 > Ztest_2023_2024.log 2>&1 &
-nohup python -u /home/hongkou/TimeSeries/GRU_GBDT_rollingtrain.py --task_name 'Jin_2024_2025_randomseed2025' --time_period '2024-2025' --exp_path '/home/hongkou/TimeSeries/exp/Jin_2024_2025_randomseed2025' --device 'cuda:4' --random_seed 2025 > Ztest_2024_2025.log 2>&1 &
+nohup python D:\chenxing\Finforecast\GRU_GBDT_rollingtrain.py --task_name 'AttGRU_244_202404_202406' --train_time_period '201901-202403' --test_time_period '202404-202406' > test1.log 2>&1 &
+nohup python D:\chenxing\Finforecast\GRU_GBDT_rollingtrain.py --task_name 'AttGRU_244_202407_202409' --train_time_period '201901-202406' --test_time_period '202407-202409' > test2.log 2>&1 &
+nohup python D:\chenxing\Finforecast\GRU_GBDT_rollingtrain.py --task_name 'AttGRU_244_202410_202412' --train_time_period '201901-202409' --test_time_period '202410-202412' > test3.log 2>&1 &
+nohup python D:\chenxing\Finforecast\GRU_GBDT_rollingtrain.py --task_name 'AttGRU_244_202501_202504' --train_time_period '201901-202412' --test_time_period '202501-202504' > test4.log 2>&1 &
 
 ##nohup python -u /home/hongkou/TimeSeries/GRU_GBDT_rollingtrain.py --task_name 'CY_2021_2022_std' --time_period '2021-2022' --device 'cuda:0' > test_2021_2022.log 2>&1 &
 #nohup python -u /home/hongkou/TimeSeries/GRU_GBDT_rollingtrain.py --task_name 'CY_2022_2023_std' --time_period '2022-2023' --device 'cuda:3' --early_stop_patience 2 > test_2022_2023.log 2>&1 &
